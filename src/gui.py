102ee4599d4c3d7e7bfc26a76bef88bf29ed8c85
domi <01152470@pw.edu.pl>
2024-01-13 00:20:03 +0100

    implement adding blocks

--- src/gui.py
+++ src/gui.py
@@ -5,10 +5,104 @@ from PyQt5.QtWidgets import (
     QComboBox,
     QDoubleSpinBox,
     QPushButton,
-    QSizePolicy
+    QSizePolicy,
+    QMessageBox,
+    QFileDialog,
+    QDialog,
+    QLineEdit,
+    QButtonGroup,
+    QRadioButton
     )
 
 import glm
+
+from src.objects.cube import Cube
+from src.objects.model_3d import Model3D
+
+
+class AddBlockWindow(QDialog):
+    def __init__(self, parent=None):
+        super(AddBlockWindow, self).__init__(parent)
+
+        self.setWindowTitle('add block')
+        self.layout = QGridLayout(self)
+        name_label = QLabel('new block name:')
+        self.name_input = QLineEdit()
+
+        type_label = QLabel('new block type:')
+        self.simple_cube = QRadioButton('default cube')
+        self.custom_model = QRadioButton('custom model')
+        self.button_group = QButtonGroup()
+        self.button_group.addButton(self.simple_cube)
+        self.button_group.addButton(self.custom_model)
+        self.simple_cube.setChecked(True)
+        self.simple_cube.toggled.connect(self.enable_input)
+
+        self.texture_path_label = QLabel('model texture path:')
+        self.texture_path_label.setVisible(False)
+        self.texture_path_input = QLineEdit()
+        self.texture_path_input.setReadOnly(True)
+        self.texture_path_input.setVisible(False)
+        self.texture_path_button = QPushButton('select')
+        self.texture_path_button.setVisible(False)
+        self.texture_path_button.clicked.connect(self.choose_texture_path)
+        self.object_path_label = QLabel('model object path:')
+        self.object_path_label.setVisible(False)
+        self.object_path_input = QLineEdit()
+        self.object_path_input.setReadOnly(True)
+        self.object_path_input.setVisible(False)
+        self.object_path_button = QPushButton('select')
+        self.object_path_button.setVisible(False)
+        self.object_path_button.clicked.connect(self.choose_object_path)
+
+        accept_label = QLabel('add block?')
+        ok_button = QPushButton('ok')
+        cancel_button = QPushButton('cancel')
+        ok_button.clicked.connect(self.accept)
+        cancel_button.clicked.connect(self.reject)
+
+        self.layout.addWidget(name_label, 0, 0)
+        self.layout.addWidget(self.name_input, 0, 1)
+        self.layout.addWidget(type_label, 1, 0)
+        self.layout.addWidget(self.simple_cube, 2, 0)
+        self.layout.addWidget(self.custom_model, 2, 1)
+        self.layout.addWidget(self.texture_path_label, 3, 0)
+        self.layout.addWidget(self.texture_path_input, 3, 1)
+        self.layout.addWidget(self.texture_path_button, 4, 1)
+        self.layout.addWidget(self.object_path_label, 5, 0)
+        self.layout.addWidget(self.object_path_input, 5, 1)
+        self.layout.addWidget(self.object_path_button, 6, 1)
+        self.layout.addWidget(accept_label, 7, 0)
+        self.layout.addWidget(ok_button, 8, 0)
+        self.layout.addWidget(cancel_button, 8, 1)
+        self.setLayout(self.layout)
+
+    def choose_texture_path(self):
+        self.choose_file_path(self.texture_path_input)
+
+    def choose_object_path(self):
+        self.choose_file_path(self.object_path_input)
+
+    def choose_file_path(self, line_edit):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        file_name, _ = QFileDialog.getOpenFileName(
+            self,
+            "Open File",
+            "",
+            "All Files (*);;Text Files (*.txt)",
+            options=options
+        )
+        if file_name:
+            line_edit.setText(file_name)
+
+    def enable_input(self):
+        self.texture_path_label.setVisible(not self.simple_cube.isChecked())
+        self.texture_path_input.setVisible(not self.simple_cube.isChecked())
+        self.texture_path_button.setVisible(not self.simple_cube.isChecked())
+        self.object_path_label.setVisible(not self.simple_cube.isChecked())
+        self.object_path_input.setVisible(not self.simple_cube.isChecked())
+        self.object_path_button.setVisible(not self.simple_cube.isChecked())
 
 
 class GUI(QWidget):
@@ -143,15 +237,26 @@ class GUI(QWidget):
         setattr(obj, f'_{property_name}', new_prop)
 
     def on_remove_button_click(self) -> None:
-        self.ge._scene.remove(self.selected_object)
-        self.selected_object.destroy()
-        self.update_dropdown()
+        button = QMessageBox.question(
+            self, 'remove block', 'are you sure you want to remove the block?'
+        )
+        if button == QMessageBox.Yes:
+            self.remove_block()
 
     def on_add_button_click(self) -> None:
-        # needs implementation
-        pass
+        add_popup = AddBlockWindow(self)
+        result = add_popup.exec()
 
-    # ====== UPDATE LOOP ====== #
+        if result == QDialog.Accepted:
+            block_name = add_popup.name_input.text()
+            if add_popup.simple_cube.isChecked():
+                self.add_cube(block_name)
+            else:
+                texture_path = add_popup.texture_path_input.text()
+                object_path = add_popup.object_path_input.text()
+                self.add_block(block_name, texture_path, object_path)
+
+    # ====== UPDATE STATE ====== #
 
     def update(self):
         if self.render_initialized is False and self.ge._scene is not None:
@@ -171,3 +276,64 @@ class GUI(QWidget):
         for obj in self.ge._scene:
             self.dropdown.addItem(obj._name)
         self.current_objects = self.ge._scene
+
+    def remove_block(self):
+        self.ge._scene.remove(self.selected_object)
+        self.selected_object.destroy()
+        self.update_dropdown()
+
+    def add_cube(self, block_name):
+        name_exists = any(obj._name == block_name for obj in self.ge._scene)
+        if block_name == "":
+            QMessageBox.critical(
+                self,
+                'add block error',
+                'block name cannot be blank',
+                QMessageBox.Ok
+            )
+        elif name_exists:
+            QMessageBox.critical(
+                self,
+                'add block error',
+                'block with that name already exists',
+                QMessageBox.Ok
+            )
+        else:
+            cube = Cube(
+                self.ge,
+                texture_path="src/textures/crate.png",
+                pos=(5, 0, 0),
+                rot=(0, 0, -45),
+                scale=(1, 1, 1),
+                name=block_name
+            )
+            self.ge._scene.append(cube)
+            self.update_dropdown()
+
+    def add_block(self, block_name, texture_path, object_path):
+        name_exists = any(obj._name == block_name for obj in self.ge._scene)
+        if block_name == "":
+            QMessageBox.critical(
+                self,
+                'add block error',
+                'block name cannot be blank',
+                QMessageBox.Ok
+            )
+        elif name_exists:
+            QMessageBox.critical(
+                self,
+                'add block error',
+                'block with that name already exists',
+                QMessageBox.Ok
+            )
+        else:
+            model = Model3D(
+                self.ge,
+                texture_path=texture_path,
+                object_path=object_path,
+                scale=(0.2, 0.2, 0.2),
+                pos=(4, 4, 4),
+                name=block_name
+            )
+            self.ge._scene.append(model)
+            self.update_dropdown()
